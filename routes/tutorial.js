38c23aabe1a276625422e36354676d5b5c2d9ee7
sangram <mail.sangram.desai@gmail.com>
2020-12-15 11:34:32 +0530

    first commit

---
+++ routes/tutorial.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var router = express.Router();
+
+const tutorials = require("../controllers/tutorial.js");
+
+var router = require("express").Router();
+
+// Create a new Tutorial
+router.post("/", tutorials.create);
+
+// Retrieve all Tutorials
+router.get("/", tutorials.findAll);
+
+// Retrieve all published Tutorials
+router.get("/published", tutorials.findAllPublished);
+
+// Retrieve a single Tutorial with id
+router.get("/:id", tutorials.findOne);
+
+// Update a Tutorial with id
+router.put("/:id", tutorials.update);
+
+// Delete a Tutorial with id
+router.delete("/:id", tutorials.delete);
+
+// Create a new Tutorial
+router.delete("/", tutorials.deleteAll);
+  
+module.exports = router;
